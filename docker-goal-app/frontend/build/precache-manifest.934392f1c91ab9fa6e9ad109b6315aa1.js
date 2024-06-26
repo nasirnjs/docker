a05fd72dbc5b1247ac02d7b3188ae2ad8d3f6376
nasirnjs <nasirnjs@gmail.com>
2024-06-26 20:54:32 +0600

    docker compose goals

---
+++ docker-goal-app/frontend/build/precache-manifest.934392f1c91ab9fa6e9ad109b6315aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc51dabccbfe36d238d46cab29d99fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b56cafbe8230d01c31",
+    "url": "/static/css/main.1828f0bb.chunk.css"
+  },
+  {
+    "revision": "2f2ad6da3beb18970790",
+    "url": "/static/js/2.1b03d8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b03d8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b56cafbe8230d01c31",
+    "url": "/static/js/main.20a4d847.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
